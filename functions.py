a1c1d22dd0c6c4fed66e851b9dac6907ca24b4d1
Christian González <114541079+CGONZALEZ991022@users.noreply.github.com>
2023-02-12 22:32:42 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,18 @@
 import yfinance as yf
+from scipy.optimize import minimize
 #Creo una función con 4 terminos para descargar los tickers solicitados
 def Descargar_Acciones_IPC(ticker, fecha_inicio, fecha_fin, Progress):
     df = yf.download(ticker, start=fecha_inicio, end=fecha_fin, progress = Progress)
     return df
+## Funciones y datos necesarios para resolver el problema de selección de portafolios
+
+# Varianza
+def var(w, Sigma):
+    return np.dot(w.T, np.dot(Sigma, w))
+
+# -Ratio de Sharpe
+def minus_RS(w, rf, er, Sigma):
+    erp = np.dot(w.T, er)
+    sp = var(w, Sigma)**0.5
+    RS = (erp - rf) / sp
+    return -RS
